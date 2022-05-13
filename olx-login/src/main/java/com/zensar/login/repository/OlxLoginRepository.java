4269e01b9e5b60f66b734add07c1b1dc2e774d51
Shravyaadepu <102963986+Shravyaadepu@users.noreply.github.com>
2022-05-13 17:36:44 +0530

    Delete olx-login directory

--- olx-login/src/main/java/com/zensar/login/repository/OlxLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.login.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zensar.login.entity.User;
-
-public interface OlxLoginRepository extends JpaRepository<User, Integer> {
-
-}
